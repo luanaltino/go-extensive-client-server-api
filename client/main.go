43f7c3da9c925a3bf30a8f245dd5a0b3e571f4a7
Luan Altino <laltino@uolinc.com>
2023-04-20 10:48:48 -0300

    criando client

---
+++ client/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"go-extensive-client-server-api/server/models"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
+	//cancelando o contexto
+	defer cancel()
+	url := "http://localhost:8080/cotacao"
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var cambioQuotation models.CambioQuotation
+	err = json.Unmarshal(body, &cambioQuotation)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(cambioQuotation)
+	writeFile(cambioQuotation.USDBRL.Bid)
+
+	defer res.Body.Close()
+
+}
+
+func writeFile(bid string) {
+	f, err := os.Create("cotacao.txt")
+	if err != nil {
+		panic(err)
+	}
+	length, err := f.Write([]byte("Valor dólar: " + bid))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Arquivo criado com sucesso, possui %v bytes", length)
+	f.Close()
+}
