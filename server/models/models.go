a12f012f2d570d05062bc72e0c65a6ea91049fd6
Luan Altino <laltino@uolinc.com>
2023-04-20 10:47:59 -0300

    adicionando models

---
+++ server/models/models.go
@@ -0,0 +1,17 @@
+package models
+
+type CambioQuotation struct {
+	USDBRL struct {
+		Code       string `json:"-"`
+		Codein     string `json:"-"`
+		Name       string `json:"-"`
+		High       string `json:"-"`
+		Low        string `json:"-"`
+		VarBid     string `json:"-"`
+		PctChange  string `json:"-"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"-"`
+		Timestamp  string `json:"-"`
+		CreateDate string `json:"-"`
+	} `json:"USDBRL"`
+}
